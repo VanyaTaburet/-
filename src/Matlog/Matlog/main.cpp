632284a436dc259e5f2371692035e0dda38592a2
VanyaTaburet <salamatovii05@gmail.com>
2025-11-30 18:16:56 +0300

    +var

--- src/Matlog/Matlog/main.cpp
+++ src/Matlog/Matlog/main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include "bdd.h"
 
+// My variant
+
+#define N 9
+#define M 4
+
+#define LOG2N 4
+
+#define n1 7
+#define n2 4
+#define n3 2
+#define n4 6
+
+// - - -
+// - 0 -
+// x x -
+// мой вид соседства
+
+// horizontal connect
+
+
+
+using namespace std;
+
+
 int main() {
+
     // Инициализация BuDDy
     bdd_init(1000, 100);
     bdd_setvarnum(2);   // две булевы переменные
+
+
 
     bdd x = bdd_ithvar(0);
     bdd y = bdd_ithvar(1);
